d3db1a39b9967cd4365416266e92d7870d6fd213
DESKTOP-LTE87PN\ezequ <erocha@tecnologiageo.com.br>
2018-03-21 05:56:47 -0300

    Just leaving the examples folder.

--- experiments/leaflet/ipyleaflet/js/src/notebook.js
+++
@@ -1,4 +0,0 @@
-// Setup notebook base URL
-__webpack_public_path__ = document.querySelector('body').getAttribute('data-base-url') + 'nbextensions/jupyter-leaflet/';
-
-module.exports = require('./index.js');
