07ca6ff63d6a8f5bfe69772be1189eb153910bbd
DESKTOP-LTE87PN\ezequ <erocha@tecnologiageo.com.br>
2018-03-25 14:04:06 -0300

    Update from DSA repo

--- DSAPythonFundamentos/Cap02/Lab01/game_v1.py
+++ DSAPythonFundamentos/Cap02/Lab01/game_v1.py
@@ -1,27 +1,36 @@
 # Game Ping-Pong
-
+#Dependencies
 from tkinter import *
 import random
 import time
 
+# input data
 level = int(input("Qual nível você gostaria de jogar? 1/2/3/4/5 \n"))
 length = 500/level
 
-
+# creating main object
 root = Tk()
+# set application title
 root.title("Ping Pong")
+# devining sizing
 root.resizable(0,0)
 root.wm_attributes("-topmost", -1)
 
+# creating drawing container
 canvas = Canvas(root, width=800, height=600, bd=0,highlightthickness=0)
 canvas.pack()
 
+# updating main class
 root.update()
 
+# initialize count variable
 count = 0
+# setting lost to false
 lost = False
 
+# creating Bola (ball) class
 class Bola:
+    # defining constructor
     def __init__(self, canvas, Barra, color):
         self.canvas = canvas
         self.Barra = Barra
@@ -37,7 +46,7 @@ class Bola:
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
 
-
+    # defining drawing function
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
 
@@ -74,7 +83,9 @@ class Bola:
             lost = True
 
 
+# defining Barra (bar) class
 class Barra:
+    # class constructor
     def __init__(self, canvas, color):
         self.canvas = canvas
         self.id = canvas.create_rectangle(0, 0, length, 10, fill=color)
@@ -112,6 +123,7 @@ class Barra:
             self.x = 3
 
 
+# global function
 def start_game(event):
     global lost, count
     lost = False
@@ -130,15 +142,16 @@ def score():
 def game_over():
     canvas.itemconfig(game, text="Game over!")
 
-
+# instantiating objects of the game (Barra and Ball)
 Barra = Barra(canvas, "orange")
 Bola = Bola(canvas, Barra, "purple")
 
-
+# instantiation score and the game
 score_now = canvas.create_text(430, 20, text="Pontos: " + str(count), fill = "green", font=("Arial", 16))
 game = canvas.create_text(400, 300, text=" ", fill="red", font=("Arial", 40))
 
-
+# setting starting point to left button of the mouse "Button-1"
 canvas.bind_all("<Button-1>", start_game)
 
+# instantiationg main function from tkinter
 root.mainloop()
